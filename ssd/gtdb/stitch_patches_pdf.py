2352a783889fc043578e3da8dd1238458ac4238c
ayushkumarshah <ayush.kumar.shah@gmail.com>
2020-09-20 14:36:20 -0400

    Add PEP 8 formatting to stitch_patches

--- ssd/gtdb/stitch_patches_pdf.py
+++ ssd/gtdb/stitch_patches_pdf.py
@@ -4,8 +4,9 @@
 
 # read the image
 import sys
-sys.path.extend(['/home/psm2208/code', '/home/psm2208/code'])
-#sys.path.extend(['/media/psm2208/Workspace/ssd', '/media/psm2208/Workspace/ssd'])
+
+# sys.path.extend(['/home/psm2208/code', '/home/psm2208/code'])
+sys.path.extend(["/home/as1211/Desktop/MathSeer/ScanSSD/ssd"])
 import cv2
 import os
 import numpy as np
@@ -20,34 +21,54 @@ import shutil
 # Default parameters for thr GTDB dataset
 def parse_args():
 
-    parser = argparse.ArgumentParser(
-        description='Stitching method')
+    parser = argparse.ArgumentParser(description="Stitching method")
 
-    parser.add_argument('--data_file', default='test',
-                        type=str, help='choose one')
-    parser.add_argument('--output_dir', default='.',
-                        help='Output directory path')
-    parser.add_argument('--math_dir', required=True,
-                        type=str, help='detections dir')
-    parser.add_argument('--math_ext', default='.csv',
-                        help='Extention of detection files')
-    parser.add_argument('--home_data', default='/home/psm2208/data/GTDB/', type = str,
-                        help='database dir')
-    parser.add_argument('--home_eval', default='/home/psm2208/code/eval/', type = str,
-                        help='Eval dir')
-    parser.add_argument('--home_images', default='/home/psm2208/data/GTDB/images/', type = str,
-                        help='Images dir')
-    parser.add_argument('--home_anno', default='/home/psm2208/data/GTDB/annotations/', type = str,
-                        help='Annotations dir')
-    parser.add_argument('--home_char', default='/home/psm2208/data/GTDB/char_annotations/', type = str,
-                        help='Char anno dir')
-    parser.add_argument('--stitching_algo', default='equal', type=str, help='Stitching algo to use')
-    parser.add_argument('--algo_threshold', default=30, type=int, help='Stitching algo threshold')
-    parser.add_argument('--num_workers', default=4, type=int, help='Number of workers')
-    parser.add_argument('--preprocess', type=bool, help='Whether to fit math regions before pooling')
-    parser.add_argument('--postprocess', type=bool, help='Whether to fit math regions after pooling')
+    parser.add_argument("--data_file", default="test", type=str, help="choose one")
+    parser.add_argument("--output_dir", default=".", help="Output directory path")
+    parser.add_argument("--math_dir", required=True, type=str, help="detections dir")
+    parser.add_argument(
+        "--math_ext", default=".csv", help="Extention of detection files"
+    )
+    parser.add_argument(
+        "--home_data", default="/home/psm2208/data/GTDB/", type=str, help="database dir"
+    )
+    parser.add_argument(
+        "--home_eval", default="/home/psm2208/code/eval/", type=str, help="Eval dir"
+    )
+    parser.add_argument(
+        "--home_images",
+        default="/home/psm2208/data/GTDB/images/",
+        type=str,
+        help="Images dir",
+    )
+    parser.add_argument(
+        "--home_anno",
+        default="/home/psm2208/data/GTDB/annotations/",
+        type=str,
+        help="Annotations dir",
+    )
+    parser.add_argument(
+        "--home_char",
+        default="/home/psm2208/data/GTDB/char_annotations/",
+        type=str,
+        help="Char anno dir",
+    )
+    parser.add_argument(
+        "--stitching_algo", default="equal", type=str, help="Stitching algo to use"
+    )
+    parser.add_argument(
+        "--algo_threshold", default=30, type=int, help="Stitching algo threshold"
+    )
+    parser.add_argument("--num_workers", default=4, type=int, help="Number of workers")
+    parser.add_argument(
+        "--preprocess", type=bool, help="Whether to fit math regions before pooling"
+    )
+    parser.add_argument(
+        "--postprocess", type=bool, help="Whether to fit math regions after pooling"
+    )
 
     return parser.parse_args()
+
 
 def read_math(args, pdf_name):
 
@@ -55,13 +76,14 @@ def read_math(args, pdf_name):
     data = np.array([])
 
     if os.path.exists(math_file):
-        data = np.genfromtxt(math_file, delimiter=',')
+        data = np.genfromtxt(math_file, delimiter=",")
 
         # if there is only one entry convert it to correct form required
         if len(data.shape) == 1:
             data = data.reshape(1, -1)
 
     return data
+
 
 def vote_for_regions(args, math_regions, image):
 
@@ -71,11 +93,11 @@ def vote_for_regions(args, math_regions, image):
 
     votes = np.zeros(shape=(original_height, original_width))
 
-    if args.stitching_algo == 'sum_score':
+    if args.stitching_algo == "sum_score":
         votes = voting_sum_score(votes, math_regions)
-    elif args.stitching_algo == 'max_score':
+    elif args.stitching_algo == "max_score":
         votes = voting_max_score(votes, math_regions)
-    elif args.stitching_algo == 'avg_score':
+    elif args.stitching_algo == "avg_score":
         votes = voting_avg_score(votes, math_regions)
     else:  # algorithm='equal'
         votes = voting_equal(votes, math_regions)
@@ -87,14 +109,17 @@ def vote_for_regions(args, math_regions, image):
 
     return votes
 
+
 def voting_sum_score(votes, math_regions):
 
     # cast votes for the regions
     for box in math_regions:
-        votes[int(box[1]):int(box[3]), int(box[0]):int(box[2])] = \
-            votes[int(box[1]):int(box[3]), int(box[0]):int(box[2])] + box[4]
+        votes[int(box[1]) : int(box[3]), int(box[0]) : int(box[2])] = (
+            votes[int(box[1]) : int(box[3]), int(box[0]) : int(box[2])] + box[4]
+        )
 
     return votes
+
 
 def voting_max_score(votes, math_regions):
 
@@ -102,17 +127,20 @@ def voting_max_score(votes, math_regions):
     data = math_regions[math_regions[:, 4].argsort()]
 
     for box in data:
-        votes[int(box[1]):int(box[3]), int(box[0]):int(box[2])] = box[4]
+        votes[int(box[1]) : int(box[3]), int(box[0]) : int(box[2])] = box[4]
 
     return votes
+
 
 def voting_equal(votes, math_regions):
     # cast votes for the regions
     for box in math_regions:
-        votes[int(box[1]):int(box[3]), int(box[0]):int(box[2])] = \
-            votes[int(box[1]):int(box[3]), int(box[0]):int(box[2])] + 1
+        votes[int(box[1]) : int(box[3]), int(box[0]) : int(box[2])] = (
+            votes[int(box[1]) : int(box[3]), int(box[0]) : int(box[2])] + 1
+        )
 
     return votes
+
 
 def voting_avg_score(votes, math_regions):
 
@@ -120,13 +148,15 @@ def voting_avg_score(votes, math_regions):
 
     # cast votes for the regions
     for box in math_regions:
-        votes[int(box[1]):int(box[3]), int(box[0]):int(box[2])] = \
-            votes[int(box[1]):int(box[3]), int(box[0]):int(box[2])] + box[4]
+        votes[int(box[1]) : int(box[3]), int(box[0]) : int(box[2])] = (
+            votes[int(box[1]) : int(box[3]), int(box[0]) : int(box[2])] + box[4]
+        )
 
     # count the regions
     for box in math_regions:
-        counts[int(box[1]):int(box[3]), int(box[0]):int(box[2])] = \
-            counts[int(box[1]):int(box[3]), int(box[0]):int(box[2])] + 1
+        counts[int(box[1]) : int(box[3]), int(box[0]) : int(box[2])] = (
+            counts[int(box[1]) : int(box[3]), int(box[0]) : int(box[2])] + 1
+        )
 
     # To avoid divide by zero
     # When counts is zero, votes will be zero
@@ -163,6 +193,7 @@ def perform_nms(math_regions):
 
     return math_regions.tolist()
 
+
 def preprocess_math_regions(math_regions, image):
 
     im_bw = convert_to_binary(image)
@@ -179,9 +210,11 @@ def preprocess_math_regions(math_regions, image):
 def voting_algo(params):
 
     args, math_regions, pdf_name, page_num = params
-    print('Processing ', pdf_name, ' > ', page_num)
+    print("Processing ", pdf_name, " > ", page_num)
 
-    image = cv2.imread(os.path.join(args.home_images,pdf_name,str(int(page_num+1))+".png"))
+    image = cv2.imread(
+        os.path.join(args.home_images, pdf_name, str(int(page_num + 1)) + ".png")
+    )
 
     if args.preprocess:
         math_regions = preprocess_math_regions(math_regions, image)
@@ -199,13 +232,18 @@ def voting_algo(params):
 
     for i in range(ncomponents):
 
-        labels = (labeled == (i+1))
+        labels = labeled == (i + 1)
         pixels = indices[labels.T]
 
         if len(pixels) < 1:
             continue
 
-        box = [min(pixels[:, 0]), min(pixels[:, 1]), max(pixels[:, 0]), max(pixels[:, 1])]
+        box = [
+            min(pixels[:, 0]),
+            min(pixels[:, 1]),
+            max(pixels[:, 0]),
+            max(pixels[:, 1]),
+        ]
 
         if args.postprocess:
             # expansion to correctly fit the region
@@ -222,7 +260,7 @@ def voting_algo(params):
 
 def stitch(args):
     pdf_list = []
-    pdf_names_file = open(args.data_file, 'r')
+    pdf_names_file = open(args.data_file, "r")
 
     if os.path.exists(args.output_dir):
         shutil.rmtree(args.output_dir)
@@ -230,7 +268,7 @@ def stitch(args):
     for pdf_name in pdf_names_file:
         pdf_name = pdf_name.strip()
 
-        if pdf_name != '':
+        if pdf_name != "":
             pdf_list.append(pdf_name)
 
     math_regions = {}
@@ -240,13 +278,16 @@ def stitch(args):
 
     voting_ip_list = []
     for pdf_name in pdf_list:
-
         pages = np.unique(math_regions[pdf_name][:, 0])
 
-        #args, math_regions, pdf_name, page_num
+        # args, math_regions, pdf_name, page_num
         for page_num in pages:
-            current_math = math_regions[pdf_name][math_regions[pdf_name][:,0] == page_num]
-            voting_ip_list.append([args, np.delete(current_math, 0, 1), pdf_name, page_num])
+            current_math = math_regions[pdf_name][
+                math_regions[pdf_name][:, 0] == page_num
+            ]
+            voting_ip_list.append(
+                [args, np.delete(current_math, 0, 1), pdf_name, page_num]
+            )
 
     pool = Pool(processes=args.num_workers)
     out = pool.map(voting_algo, voting_ip_list)
@@ -263,17 +304,26 @@ def stitch(args):
             col = np.array([int(page_num)] * len(final_math))
             final_math = np.concatenate((col[:, np.newaxis], final_math), axis=1)
 
-            math_file_path = os.path.join(args.output_dir, pdf_name + '.csv')
+            math_file_path = os.path.join(args.output_dir, pdf_name + ".csv")
 
             if not os.path.exists(os.path.dirname(math_file_path)):
                 os.makedirs(os.path.dirname(math_file_path))
 
-            math_file = open(math_file_path, 'a')
+            math_file = open(math_file_path, "a")
 
-            np.savetxt(math_file, final_math, fmt='%.2f', delimiter=',')
+            np.savetxt(math_file, final_math, fmt="%.2f", delimiter=",")
             math_file.close()
         except Exception as e:
-            print("Exception while processing ", pdf_name, " ", page_num, " ", sys.exc_info(), e)
+            print(
+                "Exception while processing ",
+                pdf_name,
+                " ",
+                page_num,
+                " ",
+                sys.exc_info(),
+                e,
+            )
+
 
 def convert_to_binary(image):
 
@@ -286,9 +336,9 @@ def convert_to_binary(image):
     return im_bw
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # TODO: use argparser
     args = parse_args()
-    print('Using : ', args)
+    print("Using : ", args)
     stitch(args)
